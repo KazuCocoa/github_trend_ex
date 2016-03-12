a7b476e5e8976655bc1e5c483bbd2974d66f4e8f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-03-12 18:15:43 +0900

    implement getting trend

--- lib/github_trend_ex.ex
+++ lib/github_trend_ex.ex
@@ -1,2 +1,62 @@
 defmodule GithubTrendEx do
+  @moduledoc """
+  GithubTrendEx get trend data from github.
+  """
+
+  alias Floki
+  alias HTTPoison
+
+  @scheme "https://"
+  @github_domain "github.com"
+  @trending_path "/trending"
+
+  @doc """
+  Get github trending from github.com
+  """
+  @spec trend() :: bitstring
+  def trend(lang \\ "") do
+    trending_url = trending_url lang
+
+    HTTPoison.get!(trending_url)
+    |> Map.get(:body)
+  end
+
+  @doc """
+  Generate URL for trending
+
+  Example
+
+      iex> GithubTrendEx.trending_url
+      "https://github.com/trending"
+
+      iex> GithubTrendEx.trending_url("elixir")
+      "https://github.com/trending/elixir"
+  """
+  @spec trending_url(bitstring) :: bitstring
+  def trending_url(lang \\ "") do
+    case lang do
+      s when is_bitstring(s) and byte_size(s) > 0->
+        @scheme <> @github_domain <> @trending_path <> "/" <> s
+      _ ->
+        @scheme <> @github_domain <> @trending_path
+    end
+  end
+
+  @doc """
+  Return Map `%{name: "a name of the trend", url: "a url to github of the trend", description: "description of the trend"}`
+  """
+  @spec list(bitstring) :: integer | nil
+  def list(html) do
+    names = Floki.find(html, ".repo-list-name a")
+    descriptions = Floki.find(html, ".repo-list-description")
+
+    Enum.zip(names, descriptions)
+    |> Enum.reduce([], fn  {{_, href, _}, {_, _, desc}}, acc ->
+      {_tag, link} = List.first(href)
+      desc = List.first(desc)
+             |> String.strip
+
+      List.flatten [acc | [%{name: link, url: @scheme <> @github_domain <> link, description: desc}]]
+    end)
+  end
 end
--- test/github_trend_ex_test.exs
+++ test/github_trend_ex_test.exs
@@ -2,7 +2,19 @@ defmodule GithubTrendExTest do
   use ExUnit.Case
   doctest GithubTrendEx
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    {:ok, [trend: File.read! "test/data/trend_sample"]}
+  end
+
+  test "the truth", context do
+    list = GithubTrendEx.list context[:trend]
+    item = List.first list
+
+    assert Enum.count(list) == 25
+    assert item.name == "/Rochester-NRT/AlphaGo"
+    assert item.url == "https://github.com/Rochester-NRT/AlphaGo"
+    assert item.description == String.strip ~s"""
+    A replication of DeepMind's 2016 Nature publication, "Mastering the game of Go with deep neural networks and tree search," details of which can be found on their website.
+    """
   end
 end
